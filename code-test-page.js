2cf52bea534211f9186cb6f6a3c819f9ea7ef420
akumaluna <ldlvvna@gmail.com>
2023-02-16 01:01:53 -0700

    goodbye-tue

--- code-test-page.js
+++ code-test-page.js
@@ -1,6 +1,20 @@
-let three = 'three';
+/*let three = 'three';
 let two = 2;
 console.log(three / two);
 
 
 console.log('run'['run'.length - 1]);
+
+
+function addThreeNumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+  }
+
+ console.log(addThreeNumbers(1, 2, 3, 4));
+*/
+
+function addThreeNumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+  }
+
+  console.log(addThreeNumbers(1, 2)); // => ???
