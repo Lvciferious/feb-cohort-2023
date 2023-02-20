ff0f41ae64ba9cf24bf9a7de8f765a2fa3111000
akumaluna <ldlvvna@gmail.com>
2023-02-20 15:49:12 -0700

    range-wed

---
+++ w1-wed-range.js
@@ -0,0 +1,35 @@
+/*
+
+Range
+Write a function range(min, max) that takes in two numbers. The function should return an array containing all numbers from min to max inclusive.
+
+Define this function using function expression syntax.
+
+*/
+
+
+//GAMEPLAN
+//non-tech solution:
+//starting from one num, include ALL nums up to the max num.
+//tech solution:
+//for loop to iterate through
+//return an array of all numbers between min andc max (inclusive)
+//BONUS: see if you can refactor with forEach or .map()!
+// your code here
+
+
+function range(min, max) {
+    let arr = [];
+    for (let i = min; i <= max; i++) {
+       arr.push(i);
+    }
+    return arr;
+}
+
+
+
+
+console.log(range(3, 10)); // [ 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(range(217, 220)); // [ 217, 218, 219, 220 ]
+console.log(range(-5, 1)); // [ -5, -4, -3, -2, -1, 0, 1 ]
+console.log(range(10, 3)); // []
